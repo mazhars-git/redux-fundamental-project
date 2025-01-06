5e9d4fafc90c3561d55f6d149eb2a81269f6914f
mazhars-git <mazharulislam611@gmail.com>
2025-01-06 07:35:09 +0600

    up

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,14 +1,48 @@
 import { AddTaskModel } from "@/components/module/tasks/AddTaskModel";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { selectTasks } from "@/redux/features/task/taskSlice";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 export default function Tasks() {
   const tasks = useSelector(selectTasks);
+
+  const dispatch = useDispatch();
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h1>Tasks</h1>
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Tasks</h1>
+
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("all"))}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("low"))}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("medium"))}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("high"))}
+              value="high"
+            >
+              High
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+
         <AddTaskModel />
       </div>
       <div className="space-y-5 mt-5">
